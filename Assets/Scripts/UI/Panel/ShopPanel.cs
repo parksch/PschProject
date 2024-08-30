8fdd2d26550f41dc50568a81c6d71c7b025a7961
parksch <33758744+parksch@users.noreply.github.com>
2024-08-30 15:48:09 +0900

    Add Shop

---
+++ Assets/Scripts/UI/Panel/ShopPanel.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopPanel : BasePanel
+{
+    [SerializeField,ReadOnly] Text title;
+
+    public override void Close()
+    {
+
+    }
+
+    public override void FirstLoad()
+    {
+
+    }
+
+    public override void Open()
+    {
+
+    }
+}

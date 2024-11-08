2c839d29af16a8c322e1483ef0293253e974d01b
parksch <33758744+parksch@users.noreply.github.com>
2024-11-08 13:04:29 +0900

    feat 장착 슬롯 스크립트 적용

--- Assets/Scripts/UI/UIEquipmentSlot.cs
+++ Assets/Scripts/UI/UIEquipmentSlot.cs
@@ -3,22 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIEquipmentSlot : MonoBehaviour
+public class UIEquipmentSlot : ItemSlot
 {
-    [SerializeField,ReadOnly] Image icon;
-    [SerializeField,ReadOnly] BaseItem target;
-
-    public void Set(BaseItem item)
-    {
-        target = item;
-
-    }
-
-    public void OnClickButton()
-    {
-        if (target.Type == ClientEnum.Item.None)
-        {
-            return;
-        }
-    }
+    [SerializeField] ClientEnum.Item itemType;
+    public ClientEnum.Item ItemType => itemType;
 }
